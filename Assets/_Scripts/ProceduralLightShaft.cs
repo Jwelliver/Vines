d4c42a099a8e1fd95638d8e3660fe727b786b489
Josh Welliver <joshwelliver@gmail.com>
2023-03-01 00:57:23 -0800

    LightShaft anim attempt

--- Assets/_Scripts/ProceduralLightShaft.cs
+++ Assets/_Scripts/ProceduralLightShaft.cs
@@ -16,14 +16,20 @@ public class ProceduralLightShaft : MonoBehaviour
     [SerializeField] float minIntensity = 1f;
     [SerializeField] float maxIntensity = 3f;
 
-
     [SerializeField] Transform shaftSpriteLight;
 
+    [SerializeField] float pctChanceNaturalAnimation = 0.2f;
+    [SerializeField] float minAnimSpeed = 0.5f;
+    [SerializeField] float maxAnimSpeed = 1f;
+
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         initShaft();
+        // StartCoroutine(checkForSizeChange()); //preventing start of this til it's ironed out
     }
 
 
@@ -40,5 +46,22 @@ public class ProceduralLightShaft : MonoBehaviour
 
         Light2D light = shaftSpriteLight.GetComponent<Light2D>();
         light.intensity = Random.Range(minIntensity,maxIntensity);
+    }
+
+
+    IEnumerator checkForSizeChange() {
+        yield return new WaitForSeconds(1f);
+        // animator.speed = Random.Range(-maxAnimSpeed,maxAnimSpeed);
+        if(Random.Range(0f,1f)<pctChanceNaturalAnimation) {
+            animator.speed = Random.Range(minAnimSpeed,maxAnimSpeed);
+        } 
+        else {
+            animator.speed = 0;
+        }
+        StartCoroutine(checkForSizeChange());
+    }
+
+    void OnDestroy() {
+        StopAllCoroutines();
     }
 }
