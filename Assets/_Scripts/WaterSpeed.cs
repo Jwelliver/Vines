5f92045d194a3247fdc9da247a7cfbb00f3e4b73
Josh Welliver <joshwelliver@gmail.com>
2023-02-28 18:44:54 -0800

    WaterSpeed controller
    Created water speed controller in attempt to control waterspeed in material from player movement (failed)

---
+++ Assets/_Scripts/WaterSpeed.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterSpeed : MonoBehaviour
+{
+    [SerializeField] Material water;
+    [SerializeField] Rigidbody2D playerRb;
+    [SerializeField] float multiplier = 0.01f;
+    [SerializeField] bool invertDirection;
+
+    float prevPlayerPosX = 0;
+
+    float currentXOffset = 0;
+
+    void Start() {
+        prevPlayerPosX =playerRb.position.x;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // float newSpeed = playerRb.velocity.x*multiplier;
+        // newSpeed = invertDirection ? -newSpeed : newSpeed;
+        // water.SetFloat("_WaterSpeed", newSpeed); //*Time.deltaTime
+        // water.SetTextureOffset("_WaterTexture", new Vector2(currentXOffset+newSpeed, 0));
+        float currentPlayerPosX = playerRb.position.x;
+        float amtPlayerMoved = prevPlayerPosX - currentPlayerPosX;
+        water.SetTextureOffset("_WaterTexture", new Vector2(currentXOffset+amtPlayerMoved, 0));
+        currentXOffset +=amtPlayerMoved;
+        prevPlayerPosX = currentPlayerPosX;
+        
+    }
+}
