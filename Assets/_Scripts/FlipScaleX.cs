dbac9b8026764538f3977beed422eb92b8a0e81e
Josh Welliver <joshwelliver@gmail.com>
2023-05-26 22:11:33 -0700

    Update FlipScaleX.cs
    code optimization

--- Assets/_Scripts/FlipScaleX.cs
+++ Assets/_Scripts/FlipScaleX.cs
@@ -8,21 +8,26 @@ public class FlipScaleX : MonoBehaviour
     Vector3 origScale; //stores orig scale on start
     private bool isFacingLeft; 
 
+    float moveInput;
+
+    Transform myTransform;
+
     void Awake() {
         origScale = transform.localScale;
+        myTransform = transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float moveInput = Input.GetAxis("Horizontal");
+        moveInput = Input.GetAxis("Horizontal");
         if (moveInput < 0 && !isFacingLeft) { 
-            transform.localScale = new Vector3(-origScale.x, origScale.y, origScale.z);
+            myTransform.localScale = new Vector3(-origScale.x, origScale.y, origScale.z);
             isFacingLeft = true;
         }
 
         else if (moveInput > 0 && isFacingLeft) {
-            transform.localScale = origScale;
+            myTransform.localScale = origScale;
             isFacingLeft = false;        }
     }
 }
