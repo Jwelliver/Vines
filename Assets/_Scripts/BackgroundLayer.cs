72cc1e82bfc190aff3850180656a963c2691302f
Josh Welliver <joshwelliver@gmail.com>
2023-09-17 21:31:18 -0700

    added BackgroundLayer.cs temp file

---
+++ Assets/_Scripts/BackgroundLayer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+091623
+
+quick monobehavior implementation of background layer generator
+
+*/
+
+public class BackgroundLayer : MonoBehaviour
+{
+    ProbabilityWeightedSpritePool sprites;
+    ProceduralLayoutParams layoutParams;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
