ba65ad95abc307f5d977d52b55d0703f4b052455
Josh Welliver <joshwelliver@gmail.com>
2023-09-19 20:04:04 -0700

    Create ProbabilityWeightedColorPool.cs

---
+++ Assets/_Scripts/ScriptableObejcts/ProbabilityWeightedColorPool.cs
@@ -0,0 +1,4 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "MyAssets/ScriptableObjects/ItemPools/ProbabilityWeightedItemPool/Colors")]
+public class ProbabilityWeightedColorPool : ProbabilityWeightedItemPool<Color> { }
