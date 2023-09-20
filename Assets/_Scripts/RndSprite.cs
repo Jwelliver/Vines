79f3f96152353c9c4ebfd6193dc672a240082c6c
Josh Welliver <joshwelliver@gmail.com>
2023-09-19 20:04:13 -0700

    Create RndSprite.cs

---
+++ Assets/_Scripts/RndSprite.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RndSprite : MonoBehaviour
+{
+    [SerializeField] ProbabilityWeightedSpritePool spritePool;
+    [SerializeField] ProbabilityWeightedColorPool colorPool;
+    [SerializeField] SpriteRenderer spriteRenderer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // If SpriteRenderer not provided, try to auto assign from this gameObject.
+        if (spriteRenderer == null)
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+        // if it's still null, log error;
+        if (spriteRenderer == null)
+        {
+            Debug.LogError("SpriteRenderer Missing on object: " + name);
+        }
+        else
+        {
+            spriteRenderer.sprite = spritePool.getRandomItem();
+            spriteRenderer.color = colorPool.getRandomItem();
+        }
+
+    }
+}
