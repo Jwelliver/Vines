03230c6fcbe3525f16a202eb809274b82c188740
Josh Welliver <joshwelliver@gmail.com>
2023-09-21 20:39:48 -0700

    Create VineAdornmentFactory

---
+++ Assets/_Scripts/ScriptableObejcts/Factories/VineAdornmentFactory.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VineAdornmentFactory : ScriptableObject
+{
+    [SerializeField] List<Transform> vineAdornmentPrefabs = new List<Transform>();
+    [SerializeField] ProbabilityWeightedSpritePool sprites;
+    [SerializeField] ProbabilityWeightedColorPool colors;
+
+    public Transform GenerateVineAdornment(Vector2 position, Transform parent, MinMax<float> scale)
+    {
+        // Get Random Adornment Prefab
+        Transform rndAdornment = RNG.RandomChoice(vineAdornmentPrefabs);
+
+        // Get Random Rotation
+        float rndRotation = RNG.RandomRange(0, 359);
+
+        // Instantiate new Vine Adornment Prefab
+        Transform newAdornment = GameObject.Instantiate(rndAdornment, position, Quaternion.Euler(0, 0, rndRotation));
+
+        // Apply Random Scale
+        float rndScale = RNG.RandomRange(scale);
+        newAdornment.localScale = new Vector2(rndScale, rndScale);
+
+        // Appy Random Sprite
+        Sprite rndSprite = sprites.getRandomItem();
+        SpriteRenderer spriteRenderer = newAdornment.GetComponent<SpriteRenderer>();
+        if (spriteRenderer == null)
+        {
+            Debug.LogError("Error: No SpriteRenderer found on adornment: " + newAdornment.name);
+            return null;
+        }
+        spriteRenderer.sprite = rndSprite;
+
+        // Apply Random Color
+        Color rndColor = colors.getRandomItem();
+        spriteRenderer.color = rndColor;
+
+        // Set Parent
+        if (parent != null) { newAdornment.SetParent(parent); }
+
+        // Return New Adornment
+        return newAdornment;
+
+    }
+
+}
