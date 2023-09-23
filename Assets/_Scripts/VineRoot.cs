b1e4f7819d6b26284c7194f56b310e5a826713cf
Josh Welliver <joshwelliver@gmail.com>
2023-09-23 00:37:07 -0700

    Update VineRoot.cs

--- Assets/_Scripts/VineRoot.cs
+++ Assets/_Scripts/VineRoot.cs
@@ -11,11 +11,30 @@ public class VineRoot : MonoBehaviour
         catch { }
     }
 
+
     public void OnVineSnap(Joint2D joint)
     {
         //TODO: Consider passing the vineSegment instance itself, or the transform;
         sfx.vineSFX.playVineSnapSound();
         GameManager.Instantiate(snapParticles, joint.attachedRigidbody.position, Quaternion.identity);
     }
+
+    // void OnBecameVisible()
+    // {
+    //     Debug.Log("VineRoot.OnBecameVisible() name: " + name);
+    //     foreach (Rigidbody2D rb in GetComponentsInChildren<Rigidbody2D>())
+    //     {
+    //         rb.WakeUp();
+    //     }
+    // }
+
+    // void OnBecameInvisible()
+    // {
+    //     Debug.Log("VineRoot.OnBecameInvisible() name: " + name);
+    //     foreach (Rigidbody2D rb in GetComponentsInChildren<Rigidbody2D>())
+    //     {
+    //         rb.WakeUp();
+    //     }
+    // }
 }
 
