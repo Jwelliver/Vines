2fe2eeeefd0bcf27c53566009430e077f243bcfb
Josh Welliver <joshwelliver@gmail.com>
2023-09-24 17:50:11 -0700

    Update ScoreSystem.cs

--- Assets/_Scripts/ScriptableObejcts/ScoreSystem.cs
+++ Assets/_Scripts/ScriptableObejcts/ScoreSystem.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 
@@ -17,27 +15,32 @@ using UnityEngine;
 public class ScoreSystem : MonoBehaviour
 {
 
+    [SerializeField] FadeText newRecordTextObj;
     [SerializeField] FadeText statsFadeTextObj;
+    [SerializeField] AudioSource recordBreakAudio;
+    [SerializeField] SimpleSave simpleSave;
     bool isRecordingSwingStats = false;
 
+    PlayerStats sessionStats;
+    // PlayerStats allTimeStats;
+
+    bool allTimeLevelDistanceBeatThisSession;
+
     Rigidbody2D playerRb;
-
-
-
 
     //Swing Stats
     Vector2 startPosition = new Vector2(0, 0); // Player start position
     Vector2 swingReleasePosition = new Vector2(0, 0);
-    float bestJumpVelocity = 0;
-    float bestJumpDistance = 0;
-    float bestJumpHeight = 0;
-    float totalDistance = 0;
-
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GameObject.Find("Player").GetComponent<Rigidbody2D>();
+        // allTimeStats = simpleSave.GetAllTimeStats();
+        sessionStats = new PlayerStats();
+        // Init/Load AllTimeStats
+        simpleSave.LoadAllTimeStats();
+        Debug.Log("Loading: " + simpleSave.RecordStats.ToString());
     }
 
     // Update is called once per frame
@@ -50,37 +53,93 @@ public class ScoreSystem : MonoBehaviour
     }
 
 
-    void UpdateSwingStats()
+    void OnNewRecord(string recordKey, float value)
+    {
+        recordBreakAudio.Play();
+        newRecordTextObj.FadeTo("New Record!!\n" + recordKey + "\n" + value.ToString());
+        simpleSave.RecordStats.Save();
+    }
+
+    void CheckForAllTimeRecord()
+    {
+        if (sessionStats.bestJumpDistance > simpleSave.RecordStats.bestJumpDistance)
+        {
+            simpleSave.RecordStats.bestJumpDistance = sessionStats.bestJumpDistance;
+            OnNewRecord("Jump Distance", sessionStats.bestJumpDistance);
+        }
+
+        if (sessionStats.bestJumpVelocity > simpleSave.RecordStats.bestJumpVelocity)
+        {
+            simpleSave.RecordStats.bestJumpVelocity = sessionStats.bestJumpVelocity;
+            OnNewRecord("Jump Velocity", sessionStats.bestJumpVelocity);
+        }
+
+        if (sessionStats.bestJumpVelocity > simpleSave.RecordStats.bestJumpVelocity)
+        {
+            simpleSave.RecordStats.bestJumpVelocity = sessionStats.bestJumpVelocity;
+            OnNewRecord("Jump Height", sessionStats.bestJumpVelocity);
+        }
+
+        if (sessionStats.bestLevelDistance > simpleSave.RecordStats.bestLevelDistance)
+        {
+            simpleSave.RecordStats.bestLevelDistance = sessionStats.bestLevelDistance;
+            // Only notify the first time it's beaten on the session, so we don't notify on every grab thereon
+            if (!allTimeLevelDistanceBeatThisSession)
+            {
+                OnNewRecord("Level Distance", sessionStats.bestLevelDistance);
+                allTimeLevelDistanceBeatThisSession = true;
+            }
+            else { simpleSave.RecordStats.Save(); }
+        }
+    }
+
+    void UpdateJumpDistance()
     {
         float jumpDistance = Vector2.Distance(swingReleasePosition, playerRb.position);
-        float currentTotalDistance = Mathf.Abs(playerRb.position.x - startPosition.x);
-        bestJumpDistance = jumpDistance > bestJumpDistance ? jumpDistance : bestJumpDistance;
-        bestJumpVelocity = (playerRb.velocity.magnitude > bestJumpVelocity) ? playerRb.velocity.magnitude : bestJumpVelocity;
-        bestJumpHeight = (playerRb.position.y > bestJumpHeight) ? playerRb.position.y : bestJumpHeight;
-        totalDistance = (currentTotalDistance > totalDistance) ? currentTotalDistance : totalDistance;
+        if (jumpDistance > sessionStats.bestJumpDistance) { sessionStats.bestJumpDistance = jumpDistance; }
     }
 
-    void resetSwingStats()
+    void UpdateJumpVelocity()
     {
-        bestJumpVelocity = 0;
-        bestJumpDistance = 0;
-        bestJumpHeight = 0;
+        float jumpVelocity = playerRb.velocity.magnitude;
+        if (jumpVelocity > sessionStats.bestJumpVelocity) { sessionStats.bestJumpVelocity = jumpVelocity; }
     }
 
-    void UpdateText()
+    void UpdateJumpHeight()
     {
-        string statsText = "Best Jump Velocity: " + bestJumpVelocity;
-        statsText += "\n" + "Best Jump Distance: " + bestJumpDistance;
-        statsText += "\n" + "Best Jump Height: " + bestJumpHeight;
-        statsText += "\n" + "Total Distance: " + totalDistance;
-        statsFadeTextObj.FadeTo(statsText);
+        float jumpHeight = playerRb.position.y;
+        if (jumpHeight > sessionStats.bestJumpHeight) { sessionStats.bestJumpHeight = jumpHeight; }
     }
 
+    void UpdateLevelDistance()
+    {
+        float levelDistance = Mathf.Abs(playerRb.position.x - startPosition.x);
+        if (levelDistance > sessionStats.bestLevelDistance) { sessionStats.bestLevelDistance = levelDistance; }
+    }
+
+    void UpdateSwingStats()
+    {
+        UpdateJumpDistance();
+        UpdateJumpHeight();
+        UpdateJumpVelocity();
+        UpdateLevelDistance();
+    }
+
+    void ResetSessionStats()
+    {
+        sessionStats.Reset();
+    }
+
+    void UpdateSessionStatsText()
+    {
+        statsFadeTextObj.FadeTo(sessionStats.ToString());
+    }
 
     public void onSwingGrab()
     {
         isRecordingSwingStats = false;
-        UpdateText();
+        UpdateSessionStatsText();
+        CheckForAllTimeRecord();
     }
 
     public void onSwingRelease()
