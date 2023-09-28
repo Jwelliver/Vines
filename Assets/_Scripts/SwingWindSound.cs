03abd711e4fcc637f7b06c78eb7c134e98e842bd
Josh Welliver <joshwelliver@gmail.com>
2023-09-28 00:31:21 -0700

    Update SwingWindSound.cs
    bit of refactor (might be related to an audio error occuring in webgl builds? Fix not tested)
    <i>Autoconnected Player "Autoconnected Player"</i> ./Modules/Audio/Public/AudioSource.cpp(1574) : Error executing dspHead->setRelativeAudibility(relativeAudibility) (An invalid object handle was used. )

--- Assets/_Scripts/SwingWindSound.cs
+++ Assets/_Scripts/SwingWindSound.cs
@@ -1,35 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class SwingWindSound : MonoBehaviour
 {
     [SerializeField] Rigidbody2D playerRb;
-    [SerializeField] SwingingController swingingController;
     [SerializeField] float minPitch = 0.5f;
     [SerializeField] float maxPitch = 1.5f;
     [SerializeField] float velocityAtMaxPitch = 10f;
 
-    [SerializeField] float minVelocityVolume = 5f; // player must be greater than this velocity before wind sound can be heard.
+    [SerializeField] float cutoffVelocity = 5f; // player must be greater than this velocity before wind sound can be heard.
     [SerializeField] float maxVolume = 50f;
     [SerializeField] float velocityAtMaxVolume = 10f;
 
     AudioSource audioSource;
-    bool isSwinging;
-    // Start is called before the first frame update
+
+    bool isPlaying;
+
     void Awake()
     {
         audioSource = GetComponent<AudioSource>();
     }
 
+    // void StartPlaying()
+    // {
+    //     isPlaying = true;
+    // }
+
+    // void StopPlaying()
+    // {
+    //     isPlaying = false;
+    //     audioSource.volume = 0;
+    // }
+
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        // handleAudioStartStop();
-        // if(isSwinging) {
-        handleAudioVolume();
-        handleAudioPitch();
-        // }
+        if (Mathf.Abs(playerRb.velocity.magnitude) > cutoffVelocity)
+        {
+            if (!isPlaying) isPlaying = true;
+        }
+        else if (isPlaying)
+        {
+            isPlaying = false;
+            audioSource.volume = 0;
+        }
+
+        if (isPlaying)
+        {
+            handleAudioVolume();
+            handleAudioPitch();
+        }
+
+
     }
 
     void handleAudioPitch()
@@ -42,28 +66,15 @@ public class SwingWindSound : MonoBehaviour
 
     void handleAudioVolume()
     {
-        float playerVelocity = playerRb.velocity.magnitude;
-        if (playerVelocity < minVelocityVolume)
-        {
-            audioSource.volume = 0;
-            return;
-        };
+        float playerVelocity = Mathf.Abs(playerRb.velocity.magnitude);
+        // if (playerVelocity < cutoffVelocity)
+        // {
+        //     audioSource.volume = 0;
+        //     return;
+        // }
         float pctOfMaxVelocity = playerVelocity / velocityAtMaxVolume;
-        float newVolume = ((maxVolume) * pctOfMaxVelocity);
+        float newVolume = maxVolume * pctOfMaxVelocity;
         audioSource.volume = newVolume;
     }
 
-    void handleAudioStartStop()
-    {
-        if (!isSwinging && SwingingController.isSwinging)
-        {
-            audioSource.Play();
-            isSwinging = true;
-        }
-        else if (isSwinging && !SwingingController.isSwinging)
-        {
-            audioSource.Stop();
-            isSwinging = false;
-        }
-    }
 }
