a717226972b5a33ff44c07b2c6e8547e0ba2e41b
Josh Welliver <joshwelliver@gmail.com>
2023-10-08 21:30:34 -0700

    FramesPerSecond.cs - Refactor/Optimization + Added time ms

--- Assets/_Scripts/FramesPerSecond.cs
+++ Assets/_Scripts/FramesPerSecond.cs
@@ -7,47 +7,39 @@ public class FramesPerSecond : MonoBehaviour
     private double lastInterval;
     private int frames = 0;
     private float fps;
-    public TMPro.TextMeshProUGUI display_Text;
-    public static bool ShowFPS;
-    // public KeyCode toggleKey = KeyCode.BackQuote;
-
+    static TMPro.TextMeshProUGUI display_Text;
+    static bool ShowFPS;
 
     void Awake()
     {
         lastInterval = Time.realtimeSinceStartup;
         frames = 0;
+        display_Text = GetComponent<TMPro.TextMeshProUGUI>();
     }
 
     public static void OnFpsButtonPressed()
     {
         ShowFPS = !ShowFPS;
+        display_Text.enabled = ShowFPS;
     }
 
-    void Update() //TODO: need a nicer way of handling this (text enabled) instead of on Udpate
+    void Update()
     {
-        if (ShowFPS)
-        {
-            if (!display_Text.enabled) { display_Text.enabled = true; }
-            updateFPS();
-        }
-        else
-        {
-            if (display_Text.enabled) { display_Text.enabled = false; }
-        }
+        if (ShowFPS) { UpdateFPS(); }
     }
 
-    void updateFPS()
+    void UpdateFPS()
     {
         ++frames;
         float timeNow = Time.realtimeSinceStartup;
+
         if (timeNow > lastInterval + updateInterval)
         {
             fps = (float)(frames / (timeNow - lastInterval));
             frames = 0;
             lastInterval = timeNow;
+            string frameTime = (Time.smoothDeltaTime * 1000).ToString("F2");
+            display_Text.text = Mathf.Ceil(fps).ToString() + " FPS (" + frameTime + "ms)";
         }
-
-        string newtext = "FPS: " + Mathf.Ceil(fps).ToString();
-        display_Text.text = newtext;
     }
 }
