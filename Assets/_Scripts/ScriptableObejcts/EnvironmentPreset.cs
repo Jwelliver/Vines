e13d17c6781ccf0dd4a215c5f0705a1f8ac34f04
Josh Welliver <joshwelliver@gmail.com>
2023-10-27 18:54:18 -0700

    Update EnvironmentPreset.cs

--- Assets/_Scripts/ScriptableObejcts/EnvironmentPreset.cs
+++ Assets/_Scripts/ScriptableObejcts/EnvironmentPreset.cs
@@ -2,6 +2,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "MyAssets/ScriptableObjects/EnvironmentPreset")]
-public class EnvironmentPreset: ScriptableObject {
+public class EnvironmentPreset : ScriptableObject
+{
+    [SerializeField] string id;
     [SerializeField] public List<EnvironmentLayer> environmentLayers = new List<EnvironmentLayer>();
+
+    // void OnEnable()
+    // {
+    //     SaveToJson();
+    // }
+
+    void SaveToJson()
+    {
+        if (id.Length == 0)
+        {
+            Debug.LogError("EnvironmentPreset.SaveToJson() Error: id must not be null (used for filename); Not saving.");
+            return;
+        }
+        string path = "JSON/EnvironmentPresets/" + id;
+        bool didSave = JsonFileIO.Save(this, path);
+        Debug.Log("EnvPreset.Save() > DidSave: " + didSave);
+    }
 }
