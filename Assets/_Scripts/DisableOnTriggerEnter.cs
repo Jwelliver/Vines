379dcffb1a39f5ee49e678b3f5b5390043901910
Josh Welliver <joshwelliver@gmail.com>
2023-12-16 18:06:10 -0800

    Create DisableOnTriggerEnter.cs
    Note: Not sure if this is in use; Created several weeks or more ago; Review

---
+++ Assets/_Scripts/DisableOnTriggerEnter.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableOnTriggerEnter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        other.gameObject.SetActive(false);
+        Debug.Log(name + " disabled by ObjectCapture trigger");
+    }
+}
