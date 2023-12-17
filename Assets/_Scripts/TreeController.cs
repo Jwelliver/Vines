f94ee47e8263d09a9c9dd0f70bbd38dae5211b3c
Josh Welliver <joshwelliver@gmail.com>
2023-12-16 18:13:05 -0800

    Create TreeController (see notes)
    WIP not in use

---
+++ Assets/_Scripts/TreeController.cs
@@ -0,0 +1,25 @@
+/*
+102223
+
+    Made to handle tree shake, and rig management;
+*/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
